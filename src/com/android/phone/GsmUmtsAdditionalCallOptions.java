Euphoria-OS/android_packages_services_Telephony

Telephony: make call waiting option a switch preference
Change-Id: Idb46fbe8a338c8ae6df3e5c218cfc5c0921327fe

src/com/android/phone/GsmUmtsAdditionalCallOptions.java
@@ -22,7 +22,7 @@ public class GsmUmtsAdditionalCallOptions extends TimeConsumingPreferenceActivit
     private static final String BUTTON_PN_KEY    = "button_pn_key";
 
     private CLIRListPreference mCLIRButton;
-    private CallWaitingCheckBoxPreference mCWButton;
+    private CallWaitingSwitchPreference mCWButton;
     private MSISDNEditPreference mMSISDNButton;
 
     private final ArrayList<Preference> mPreferences = new ArrayList<Preference>();
@@ -43,7 +43,7 @@ public class GsmUmtsAdditionalCallOptions extends TimeConsumingPreferenceActivit
 
         PreferenceScreen prefSet = getPreferenceScreen();
         mCLIRButton = (CLIRListPreference) prefSet.findPreference(BUTTON_CLIR_KEY);
-        mCWButton = (CallWaitingCheckBoxPreference) prefSet.findPreference(BUTTON_CW_KEY);
+        mCWButton = (CallWaitingSwitchPreference) prefSet.findPreference(BUTTON_CW_KEY);
         mMSISDNButton = (MSISDNEditPreference) prefSet.findPreference(BUTTON_PN_KEY);
 
         mPreferences.add(mCLIRButton);
@@ -90,8 +90,8 @@ public class GsmUmtsAdditionalCallOptions extends TimeConsumingPreferenceActivit
         if (mInitIndex < mPreferences.size()-1 && !isFinishing()) {
             mInitIndex++;
             Preference pref = mPreferences.get(mInitIndex);
-            if (pref instanceof CallWaitingCheckBoxPreference) {
-                ((CallWaitingCheckBoxPreference) pref).init(this, false, mPhone);
+            if (pref instanceof CallWaitingSwitchPreference) {
+                ((CallWaitingSwitchPreference) pref).init(this, false, mPhone);
             } else if (pref instanceof MSISDNEditPreference) {
                 ((MSISDNEditPreference) pref).init(this, false);
             }

Lucasili/metallium_android_packages_services_Telephony

src/com/android/phone/GsmUmtsAdditionalCallOptions.java
@@ -25,7 +25,7 @@ public class GsmUmtsAdditionalCallOptions extends
     private static final String BUTTON_PN_KEY    = "button_pn_key";
 
     private CLIRListPreference mCLIRButton;
-    private CallWaitingCheckBoxPreference mCWButton;
+    private CallWaitingSwitchPreference mCWButton;
     private MSISDNEditPreference mMSISDNButton;
 
     private final ArrayList<Preference> mPreferences = new ArrayList<Preference>();
@@ -41,7 +41,7 @@ public class GsmUmtsAdditionalCallOptions extends
         if (DBG) Log.d(LOG_TAG, "GsmUmtsAdditionalCallOptions onCreate, phoneId: " + mPhoneId);
         PreferenceScreen prefSet = getPreferenceScreen();
         mCLIRButton = (CLIRListPreference) prefSet.findPreference(BUTTON_CLIR_KEY);
-        mCWButton = (CallWaitingCheckBoxPreference) prefSet.findPreference(BUTTON_CW_KEY);
+        mCWButton = (CallWaitingSwitchPreference) prefSet.findPreference(BUTTON_CW_KEY);
 
         mPreferences.add(mCLIRButton);
         mPreferences.add(mCWButton);
@@ -90,8 +90,8 @@ public class GsmUmtsAdditionalCallOptions extends
         if (mInitIndex < mPreferences.size()-1 && !isFinishing()) {
             mInitIndex++;
             Preference pref = mPreferences.get(mInitIndex);
-            if (pref instanceof CallWaitingCheckBoxPreference) {
-                ((CallWaitingCheckBoxPreference) pref).init(this, false, mPhoneId);
+            if (pref instanceof CallWaitingSwitchPreference) {
+                ((CallWaitingSwitchPreference) pref).init(this, false, mPhoneId);
             } else if (pref instanceof MSISDNEditPreference) {
                 ((MSISDNEditPreference) pref).init(this, false, mPhoneId);
             }